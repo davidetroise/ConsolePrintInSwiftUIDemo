ba71b0dc30b0791f66b36873ce9cf8f5c018ec76
Davide Troise <davide@troise.net>
2022-11-29 14:49:46 +0100

    Initial Commit

---
+++ ConsolePrintInSwiftUIDemo/ConsolePrintInSwiftUIDemoApp.swift
@@ -0,0 +1,18 @@
+//
+//  ConsolePrintInSwiftUIDemoApp.swift
+//  ConsolePrintInSwiftUIDemo
+//
+//  Created by Davide Troise with Swift 5.0
+//  for the YouTube channel "Coding con Davide" https://bit.ly/3QJziJE
+//
+
+import SwiftUI
+
+@main
+struct ConsolePrintInSwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
