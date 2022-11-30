f6647d4668b3c97bceb5c687f14f15a3d2ffe81a
Davide Troise <davide@troise.net>
2022-11-30 15:36:26 +0100

    Complete code

--- ConsolePrintInSwiftUIDemo/ContentView.swift
+++ ConsolePrintInSwiftUIDemo/ContentView.swift
@@ -9,12 +9,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isOn: Bool = false
     var body: some View {
         VStack {
+            let _ = print("Inside the VSTACK")
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+            Toggle(isOn: $isOn) {
+                Text("Toggle")
+            }
+            .onChange(of: isOn) { newValue in
+                print("Toggle is \(newValue)")
+            }
+        }
+        .onAppear{
+            print("VSTACK appeared")
         }
         .padding()
     }
